09a5a98d35920e7927db26bbd93329360f9f5251
Aditya Mukerjee <dev@chimeracoder.net>
2013-04-03 17:06:31 -0400

    Insert default Author name if RSS/Atom feed does not provide it

--- server.go
+++ server.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	rss "./go-pkg-rss"
+	"encoding/csv"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/gorilla/pat"
 	"github.com/gorilla/sessions"
 	"html/template"
-	"io/ioutil"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 )
 
@@ -37,8 +36,8 @@ var (
 	//APP_SECRET = os.Getenv("APP_SECRET")
 )
 
-func scrapeRss(uri string) {
-	feed := rss.New(RSS_TIMEOUT, true, chanHandler, itemHandler)
+func scrapeRss(uri string, author string) {
+	feed := rss.New(RSS_TIMEOUT, true, chanHandler, customItemHandler(author))
 	for {
 		if err := feed.Fetch(uri, nil); err != nil {
 			fmt.Fprintf(os.Stderr, "[e.fetch] %s: %s", uri, err)
@@ -54,10 +53,22 @@ func chanHandler(feed *rss.Feed, newchannels []*rss.Channel) {
 	log.Printf("Found %d new channel(s) in %s", len(newchannels), feed.Url)
 }
 
-func itemHandler(feed *rss.Feed, ch *rss.Channel, newitems []*rss.Item) {
-	log.Printf("Found %d new item(s) in %s", len(newitems), feed.Url)
-	for _, item := range newitems {
-		savePost(*item)
+
+func customItemHandler(author string) func(*rss.Feed, *rss.Channel, []*rss.Item) {
+	return func(feed *rss.Feed, ch *rss.Channel, newitems []*rss.Item) {
+		log.Printf("Found %d new item(s) in %s", len(newitems), feed.Url)
+		for _, item := range newitems {
+			log.Printf("Item is %+v", item)
+			log.Printf("Item author %v", item.Author)
+
+			//If the author's name isn't defined, we should add it
+			if item.Author.Name == "" {
+				item.Author.Name = author
+			}
+
+			log.Printf("Item author %v", item.Author)
+			//savePost(*item)
+		}
 	}
 }
 
@@ -118,6 +129,22 @@ func serveFeeds(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+func parseFeeds(filename string) ([][]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.Comma = '\t'
+	rows, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	return rows, err
+}
+
 func main() {
 
 	var err error
@@ -143,20 +170,21 @@ func main() {
 	}
 	mongodb_session.DB(MONGODB_DATABASE).C("blogposts").EnsureIndex(guid_index)
 
-	//Read a list of all fellows' rss feeds from a file
-	bts, err := ioutil.ReadFile(FEEDS_LIST_FILENAME)
+	feeds, err := parseFeeds(FEEDS_LIST_FILENAME)
 	if err != nil {
 		panic(err)
 	}
 
-	feed_urls := strings.Split(strings.Trim(string(bts), "\n\r"), "\n")
-
-	//Set off a separate goroutine for each fellow's blog to keep it continuously up-to-date
-	for _, feed_url := range feed_urls {
+	for _, feed_info := range feeds {
+		if len(feed_info) != 2 {
+			panic(fmt.Errorf("Expect csv with 2 elements per row; received row with %d elements", len(feed_info)))
+		}
+		feed_url := feed_info[0]
+		feed_author := feed_info[1]
 		log.Printf("Found %s", feed_url)
-		go func(uri string) {
-			//scrapeRss(uri)
-		}(feed_url)
+		go func(uri string, author string) {
+			scrapeRss(uri, author)
+		}(feed_url, feed_author)
 	}
 
 	//Order of routes matters
