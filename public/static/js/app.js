af86a05d61f9697f799599112bb3ea6322b7d4d4
Jennifer Rubinovitz <jrubinovitz@gmail.com>
2013-04-14 22:22:19 -0400

    remove console.log

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -3,7 +3,6 @@ $.ajax({url:"/authors/all",
 		console.log(data);
 		for (i=0; i<data.length; i++){
 			var html = "<li><a style='color:maroon' href="+data[i].Uri+">"+data[i].Name+"</a></li>";
-			console.log(html);
 			$(".link-list").append(html);
 		}
 	}
