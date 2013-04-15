1cf42e0d4d2152d45973e7d509f403a22677b664
Dan Mundy <drm.mundy@gmail.com>
2013-04-15 12:24:18 -0400

    Fixes #11 with templating instead of AJAX

--- public/static/js/app.js
+++
@@ -1,8 +0,0 @@
-$.ajax({url:"/authors/all", 
-	success:function(data){
-		for (i=0; i<data.length; i++){
-			var html = "<li><a href="+data[i].Uri+">"+data[i].Name+"</a></li>";
-			$(".link-list").append(html);
-		}
-	}
-});
