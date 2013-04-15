a47261f7f15b01ceb6926e7bd75d490597a871a4
Dan Mundy <drm.mundy@gmail.com>
2013-04-15 10:57:53 -0400

    Makes links outside the header red

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,7 +1,7 @@
 $.ajax({url:"/authors/all", 
 	success:function(data){
 		for (i=0; i<data.length; i++){
-			var html = "<li><a style='color:maroon' href="+data[i].Uri+">"+data[i].Name+"</a></li>";
+			var html = "<li><a href="+data[i].Uri+">"+data[i].Name+"</a></li>";
 			$(".link-list").append(html);
 		}
 	}
